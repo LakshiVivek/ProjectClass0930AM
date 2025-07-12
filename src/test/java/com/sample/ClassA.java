40e012cfbc2f50c33569359b30b939814b2c3593
LakshiVivek <lakshi.vivek@gmail.com>
2025-07-12 10:41:54 +0530

    First time push

---
+++ src/test/java/com/sample/ClassA.java
@@ -0,0 +1,20 @@
+package com.sample;
+
+public class ClassA {
+	
+	private void java() {
+		System.out.println("Java");
+
+	}
+	
+	private void python() {
+		System.out.println("Selenium");
+	}
+	
+	private void ruby() {
+		System.out.println("Ruby");
+
+	}
+	
+
+}
